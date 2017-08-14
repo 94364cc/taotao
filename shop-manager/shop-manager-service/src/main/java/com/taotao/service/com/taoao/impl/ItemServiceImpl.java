5dae633d33907506854f34fdf88b134438b57a1d
zcc <386927642@qq.com>
2017-08-14 22:03:02 +0800

    20170814

--- shop-manager/shop-manager-service/src/main/java/com/taotao/service/com/taoao/impl/ItemServiceImpl.java
+++ shop-manager/shop-manager-service/src/main/java/com/taotao/service/com/taoao/impl/ItemServiceImpl.java
@@ -1,11 +1,17 @@
 package com.taotao.service.com.taoao.impl;
 
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.EUDataGridResult;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/8/6.
@@ -20,5 +26,18 @@ public class ItemServiceImpl implements ItemService {
     public TbItem getItemById(long itemId) {
         TbItem item=itemMapper.selectByPrimaryKey(itemId);
         return item;
+    }
+
+    @Override
+    public EUDataGridResult getItemList(int page, int rows) {
+        TbItemExample itemExample=new TbItemExample();
+        PageHelper.startPage(page, rows);
+        List<TbItem> list=itemMapper.selectByExample(itemExample);
+        EUDataGridResult result=new EUDataGridResult();
+        result.setRows(list);
+        PageInfo<TbItem> pageInfo=new PageInfo<>(list);
+        result.setTotal(pageInfo.getTotal());
+        return result;
+
     }
 }
--- shop-manager/shop-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ shop-manager/shop-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,12 +1,17 @@
 package com.taotao.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.taotao.pojo.EUDataGridResult;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/8/6.
@@ -15,10 +20,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ItemController {
     @Autowired
     private ItemService itemService;
-    @RequestMapping("/Item/{itemId}")
+
+
+    @RequestMapping("/item/{itemId}")
     @ResponseBody
     public TbItem getItemById(@PathVariable Long itemId){
         TbItem item=itemService.getItemById(itemId);
         return item;
+    }
+
+    @RequestMapping("/item/list")
+    @ResponseBody
+    public EUDataGridResult getItemList(int page,int rows){
+        return itemService.getItemList(page, rows);
     }
 }
