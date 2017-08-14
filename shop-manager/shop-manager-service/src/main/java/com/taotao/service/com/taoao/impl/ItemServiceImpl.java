e219c7ee400ef3264046ea74e4cba1d6670cc13d
zcc <386927642@qq.com>
2017-08-14 19:46:51 +0800

    20170814

---
+++ shop-manager/shop-manager-service/src/main/java/com/taotao/service/com/taoao/impl/ItemServiceImpl.java
@@ -0,0 +1,24 @@
+package com.taotao.service.com.taoao.impl;
+
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/8/6.
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private TbItemMapper itemMapper;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+        TbItem item=itemMapper.selectByPrimaryKey(itemId);
+        return item;
+    }
+}
